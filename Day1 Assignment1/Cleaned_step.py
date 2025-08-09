fbf8f2123c8bb74da2d93e3240ee1888bfcbaf37
AbhishekSinghBisht4 <bishtmlai2@gmail.com>
2025-08-09 19:12:53 +0530

    Delete Day1 Assignment1 directory

--- Day1 Assignment1/Cleaned_step.py
+++
@@ -1,8 +0,0 @@
-#using and doing futher steps with cleaned data.
-import pandas as pd
-data=pd.read_csv("Titanic_cleaned_dataset.csv")
-print(data.head())
-if 'Sex' in data.columns:
-    data["Sex"]=data['Sex'].map({"male":0, 'female':1})
-print(data[['Sex']].head())
-data.to_csv("Categorical to numerical.csv")
